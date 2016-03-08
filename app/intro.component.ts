e8c3b12564d44e5ab5b0ba6c79607d077c7dad1a
Jacob Wasilkowski <jwasilkowski@esri.com>
2016-03-07 18:28:19 -0800

    intro text update and misc formatting

--- app/intro.component.ts
+++ app/intro.component.ts
@@ -8,14 +8,15 @@ import { Component } from 'angular2/core';
             Features
             <ul>
                 <li><a href="https://developers.arcgis.com/javascript/beta/">Esri 4.x JSAPI</a> for 2D and 3D mapping</li>
-                <li>Angular 2 with routing
+                <li><a href="https://github.com/Esri/esri-system-js">esri-system-js</a> for loading Esri modules in SystemJS</li>
+                <li>Angular 2 app with routing
                     <ul>
-                        <li>Map view and scene view components</li>
-                        <li>Custom view coordination service to keep scene views in sync</li>
+                        <li>MapView and SceneView components</li>
+                        <li>Custom view coordination service to keep SceneView components in sync</li>
                         <li>Vector GIS analysis in the browser</li>
                     </ul>
                 </li>
-                <li><a href="http://picnicss.com/">Picnic CSS</a></li>
+                <li>and <a href="http://picnicss.com/">Picnic CSS</a> for the good looks</li>
             </ul>
         </div>
         `,
