cd37c1fece82eeacf0a4168ede0e9eb815ae5b7a
Jacob Wasilkowski <jwasilkowski@esri.com>
2016-03-22 13:16:57 -0500

    switching to hash location strategy

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,15 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+// use the hash url location strategy, see:
+//  - https://angular.io/docs/ts/latest/guide/router.html#!#-hashlocationstrategy-
+//  - http://julienrenaux.fr/2015/12/25/angular2-series-routing/#HashLocationStrategy
+import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
+import { provide } from 'angular2/core';
+
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, {
+        useClass: HashLocationStrategy
+    })
+]);
